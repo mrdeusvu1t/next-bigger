57c37ae045b1bd5094b02d713f453de52e757564
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-16 14:17:04 +0300

    Up

--- NextBiggerTask/NumberExtension.cs
+++ NextBiggerTask/NumberExtension.cs
@@ -1,20 +1,73 @@
-using System;
+﻿using System;
 
 namespace NextBiggerTask
 {
     public static class NumberExtension
     {
-        /// <summary>
-        /// Finds the nearest largest integer consisting of the digits of the given positive integer number and null if no such number exists.
-        /// </summary>
-        /// <param name="number">Source number.</param>
-        /// <returns>
-        /// The nearest largest integer consisting of the digits  of the given positive integer and null if no such number exists.
-        /// </returns>
-        /// <exception cref="ArgumentException">Thrown when source number is less than 0.</exception>
         public static int? NextBiggerThan(int number)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (number < 0)
+            {
+                throw new ArgumentException("error");
+            }
+
+            if (number < 10)
+            {
+                return null;
+            }
+
+            if (number >= int.MaxValue)
+            {
+                return null;
+            }
+
+            char[] numbers = number.ToString().ToCharArray();
+
+            string n = "";
+
+            var len = numbers.Length - 1;
+            var count = 0;
+            var x = 1;
+
+            for (int i = len; i > 0; i--)
+            {
+                if (numbers[i] > numbers[i - 1])
+                {
+                    var temp = numbers[i];
+                    numbers[i] = numbers[i - 1];
+                    numbers[i - 1] = temp;
+                    count = i;
+                    break;
+                }
+
+                x++;
+            }
+
+            if (x == numbers.Length)
+            {
+                return null;
+            }
+
+            for (int i = count; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        var temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            foreach (char num in numbers)
+            {
+                n += num;
+            }
+
+            return Convert.ToInt32(n);
+
         }
     }
 }
